a1f5b17aad5dff3e1ac14e63414843d15267867e
vikastiwari9701 <128325552+vikastiwari9701@users.noreply.github.com>
2023-03-27 23:23:42 +0530

    java files

---
+++ UnaryOperatorExample.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class UnaryOperatorExample {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		
+		int c= a+  a++ ;
+		
+		
+		System.out.println(c);
+
+	}
+
+}
